10602510ab06f64f5b2dc363863d9c83756f052a
git-alexis <alex59.billemont@gmail.com>
2026-04-20 16:07:30 +0200

    Ajout de la page de login

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'screens/login_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,13 +13,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text('Holbegram 🔥'),
-        ),
-      ),
+      home: LoginScreen(),
     );
   }
 }
