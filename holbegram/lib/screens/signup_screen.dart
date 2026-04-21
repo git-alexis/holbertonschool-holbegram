60d0d193d8611734f215664873851dbf10112431
git-alexis <alex59.billemont@gmail.com>
2026-04-21 14:03:03 +0200

    Ajout des liaisons de navigation entre les Widget connection et création de compte

--- holbegram/lib/screens/signup_screen.dart
+++ holbegram/lib/screens/signup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/text_field.dart';
+import 'login_screen.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -132,7 +133,14 @@ class _SignUpState extends State<SignUp> {
                       children: [
                         const Text("Have an account? "),
                         TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const LoginScreen(),
+                              ),
+                            );
+                          },
                           child: const Text(
                             "Log in",
                             style: TextStyle(
